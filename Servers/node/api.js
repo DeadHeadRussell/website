e7f65e399586df412d72dc8c7f614c02001fb39d
Andrew Russell <deadhead.russell@gmail.com>
2013-11-17 16:17:57 -0500

    Added basic error handling for api.js as well as better api key handling.

--- Servers/node/api.js
+++ Servers/node/api.js
@@ -4,28 +4,40 @@ var apis = {
   'getReadme': getReadme
 };
 
+var GITHUB_TOKEN = '';
+
 function getReadme(repo, callback) {
   var readme_url = 'https://api.github.com/repos/' + repo + '/readme';
   var markdown_url = 'https://api.github.com/markdown/raw';
 
   var headers = {
-    'Authorization': 'token ' // PUT TOKEN HERE
+    'Authorization': 'token ' + GITHUB_TOKEN
   };
 
   // TODO: Check if rate limited, and if so, return error status.
-  request(readme_url, function(error, response, body) {
+  var readme_options = {
+    url: readme_url,
+    headers: headers
+  };
+
+  request(readme_options, function(error, response, body) {
     var response = JSON.parse(body);
+
+    if (!response.content) {
+      callback(createContent('Error fetching README', 'text/plain'));
+    }
+
     var markdown = new Buffer(response.content.replace(/\n/g, ''), 'base64').toString('ascii');
 
     headers['Content-Type'] = 'text/plain';
 
-    var options = {
+    var markdown_options = {
       url: markdown_url,
       body: markdown,
       headers: headers
     };
 
-    request.post(options, function(error, response, body) {
+    request.post(markdown_options, function(error, response, body) {
       callback(createContent(body, 'text/html'));
     });
   });
