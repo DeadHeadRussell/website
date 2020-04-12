7d6edc1a2b663ed708dbf2677e105f6562b6490c
Andrew Russell <deadhead.russell@gmail.com>
2020-04-11 21:22:36 -0400

    Use song name instead of link in archives.

--- zipScript.py
+++ zipScript.py
@@ -1,12 +1,25 @@
 #! /usr/bin/env python3
 
+import json
 import os
 import sys
 import zipfile
 
 
+data = json.load(open('./data.json', mode='r'))
+
+
 def pretty_name(album_path):
     return os.path.basename(album_path)
+
+def find_song(album_link, song_link):
+    return next(
+        song
+        for category in data['categories']
+        for album in category.get('albums', [])
+        for song in album.get('songs', [])
+        if song['link'] == song_link and album['link'] == album_link
+    )
 
 def archive_file(archive, file_path, archive_name):
     with archive.open(archive_name, mode='w') as archive_file:
@@ -17,13 +30,18 @@ def archive(album_path):
     archive_path = os.path.join(album_path, 'archive.zip')
     art_path = os.path.join(album_path, 'art.jpg')
     songs_path = os.path.join(album_path, 'songs')
+
+    album_link = os.path.basename(album_path)
+
     with zipfile.ZipFile(archive_path, mode='w') as archive:
         archive_file(archive, art_path, os.path.basename(art_path))
 
         song_names = os.listdir(songs_path)
         for song_name in song_names:
             song_path = os.path.join(songs_path, song_name)
-            archive_file(archive, song_path, song_name)
+            song_link, ext = os.path.splitext(song_name)
+            song = find_song(album_link, song_link)
+            archive_file(archive, song_path, song['name'] + ext)
 
 def write_archives(folder_paths):
     for album_path in folder_paths:
