dbe797eabcd7bf7e5dde77b8d604ad85ad7913bf
Andrew Russell <deadhead.russell@gmail.com>
2020-05-26 00:15:48 -0400

    Add basic query API for listens.

--- src/pages/api/listens.ts
+++ src/pages/api/listens.ts
@@ -30,9 +30,24 @@ async function getListensHandler(req, res) {
     if (!isSecure(req)) {
       res.status(400).json({message: 'Invalid token'});
     } else {
-      if (req.query.count) {
-        const count = await getListensCount(req.query.album, req.query.song);
-        res.status(200).json({count});
+      if (req.query.group) {
+        const listens = await getListens();
+        const groupParser = parseGroupQuery(req.query.group);
+        const aggregateParser = parseAggregateQuery(req.query.aggregate);
+
+        if (!groupParser || !aggregateParser) {
+          res.status(400).json({message: 'Invalid query params'});
+          return;
+        }
+
+        const groupedListens = listens.reduce((grouped, listen) => {
+          const groupKey = groupParser(listen);
+          return {
+            ...grouped,
+            [groupKey]: aggregateParser(grouped[groupKey], listen)
+          };
+        }, {});
+        res.status(200).json(groupedListens);
       } else {
         const listens = await getListens();
         res.status(200).json(listens);
@@ -41,5 +56,27 @@ async function getListensHandler(req, res) {
   } catch (err) {
     res.status(500).json({error: err.message});
   }
+}
+
+function parseGroupQuery(groupQuery) {
+  if (groupQuery === 'ip') {
+    return listen => listen['user_ip'];
+  } else if (groupQuery === 'song') {
+    return listen => listen['album_link'] + ' - ' + listen['song_link'];
+  } else if (groupQuery === 'album') {
+    return listen => listen['album_link'];
+  } else if (groupQuery === 'date') {
+    return listen => new Date(listen['date']).toISOString().split('T')[0];
+  }
+  return null;
+}
+
+function parseAggregateQuery(aggregateQuery) {
+  if (aggregateQuery === 'count') {
+    return (group, listen) => (group || 0) + 1;
+  } else if (aggregateQuery === 'concat' || !aggregateQuery) {
+    return (group, listen) => (group || []).concat([listen]);
+  }
+  return null;
 }
 
--- src/services/listens.ts
+++ src/services/listens.ts
@@ -38,29 +38,3 @@ export function getListens() {
   });
 }
 
-export function getListensCount(album, song) {
-  return new Promise((resolve, reject) => {
-    if (album && song) {
-      db.get('SELECT count(*) as count FROM listens WHERE album_link = $album AND song_link = $song', {
-        $album: album,
-        $song: song
-      }, function(err, count) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(count.count);
-        }
-      });
-    } else if (!album && !song) {
-      db.get('SELECT count(*) as count FROM listens', function(err, count) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(count.count);
-        }
-      });
-    } else {
-      reject(new Error('Invalid query'));
-    }
-  });
-}
